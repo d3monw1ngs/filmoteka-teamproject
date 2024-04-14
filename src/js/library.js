b2f962307dbd205d24c447b507a9207739ddcdd8
d3monw1ngs <tezzielumsit87@gmail.com>
2024-04-14 16:49:08 +0800

    Update library.js

--- src/js/library.js
+++ src/js/library.js
@@ -4,7 +4,7 @@ const API_KEY = 'api_key=b5e824a3d922f68ba211fcf6dbdcb6f5';
 const API_URL = BASE_URL + '/discover/movie?sort_by-popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?' + API_KEY;
-const getGenres = BASE_URL + '/genre/movie/list' + API_KEY;
+const getGenres = BASE_URL + '/genre/movie/list?' + API_KEY;
 
 
 const options = {
